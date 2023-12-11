b4a35ab43d44633b8396dfff3834ef9445e29268
dorianmignonepita <dorian.mignon@epita.fr>
2023-12-11 16:58:59 +0100

    test complete

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
-cd match_manager && npm test
-echo "Match Manager tests Done" && cd ..
-cd my-express-api && npm test
-echo "My Express API tests Done"  && cd ..
-cd notifyer && npm test
-echo "Notifyer tests passed" && cd ..
+npm install jest
+cd match_manager && npm install && npm test
+echo "Match Manager tests Done" && rm -rf node_modules && cd ..
+cd my-express-api && npm install && npm test
+echo "My Express API tests Done"  && rm -rf node_modules && cd ..
+cd notifyer && npm install && npm test
+echo "Notifyer tests passed" && rm -rf node_modules && cd ..
