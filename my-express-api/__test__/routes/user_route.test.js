24ed2d3c5d5314b021dfc8464aec11d80653f203
Lenoic CRAMON <lenoic.cramon@epita.fr>
2023-12-12 12:44:18 +0100

    Add get route tests use

--- my-express-api/__test__/routes/user_route.test.js
+++ my-express-api/__test__/routes/user_route.test.js
@@ -1,12 +1,53 @@
 const request = require('supertest');
 const app = require('../../src/app');
 const db = require('../../src/loaders/mySql');
-const {getMatches, getEvents } = require('../../src/models/match')
-const { getMatchEvents, getAvailables } = require('../../src/services/match');
+const { getUsers, getFavorites, addFavorite, deleteFavorite } = require('../../src/models/user')
 
-jest.mock('../../src/models/match', () => {
+jest.mock('../../src/models/user', () => {
     return {
-        getMatches: jest.fn(),
-        getEvents: jest.fn(),
+        getUsers: jest.fn(),
+        getFavorites: jest.fn(),
+        addFavorite: jest.fn(),
+        deleteFavorite: jest.fn(),
     };
+});
+
+jest.mock('../../src/loaders/mySql', () => ({
+    query: jest.fn(),
+  }));
+
+describe('Test des routes user', () => {
+    beforeEach(() => {
+        getUsers.mockClear();
+        getFavorites.mockClear();
+        addFavorite.mockClear();
+        deleteFavorite.mockClear();
+        db.query.mockClear();
+    });
+
+    it('Test de la route get /users', async () => {
+        getUsers.mockResolvedValue([
+            { id: 1,    name: 'Jamie Tartt'},
+            { id: 2,    name: 'Roy Kent'},
+            { id: 3,    name: 'Sam Obisanya'},
+            { id: 4,    name: 'Dani Rojas'},
+            { id: 5,    name: 'Moe Bumbercatch'}
+        ]);
+
+        const response = await request(app).get(`/users`);
+        expect(response.statusCode).toEqual(200);
+        expect(response.body.length).toEqual(5);
+    });
+
+    it('Test de la route get /users/:id/favorites', async () => {
+        getFavorites.mockResolvedValue([
+            { id: 1,    userID: 1, matchID: 1},
+            { id: 2,    userID: 1, matchID: 2},
+            { id: 3,    userID: 2, matchID: 1},
+        ])
+
+        const response = await request(app).get(`/users/1/favorites`);
+        expect(response.statusCode).toEqual(200);
+        expect(response.body.length).toEqual(2);
+    });
 });
